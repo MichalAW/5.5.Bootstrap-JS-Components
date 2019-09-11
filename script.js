0bf7a9f87c9880f5c7261c885e7144c9b522d825
Michał Wanta <michal.wanta@euco.pl>
2019-09-11 17:21:38 +0200

    old archivized project

---
+++ script.js
@@ -0,0 +1,18 @@
+[class *= "example"] {
+    min - height: 300 px;
+}
+.example - 1 {
+        background - color: #59ABE3;
+
+}
+
+.example-2 {
+
+	background-color:# 2 ECC71;
+    }
+    .example - 3 {
+        background - color: #F89406;
+    }
+    .example - 4 {
+        background - color: #F22613;
+    }
